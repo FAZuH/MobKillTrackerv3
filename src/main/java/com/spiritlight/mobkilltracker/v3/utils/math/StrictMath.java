75b4f6078ac124706eeaaf7499f3ea6faadf679a
FAZuH <mail@fazuh.com>
2026-04-05 22:35:43 +0700

    chore: fix strictfp warning
    stfu "Floating-point expressions are always strictly evaluated from
    source level 17. Keyword 'strictfp' is not required."

--- src/main/java/com/spiritlight/mobkilltracker/v3/utils/math/StrictMath.java
+++ src/main/java/com/spiritlight/mobkilltracker/v3/utils/math/StrictMath.java
@@ -2,18 +2,18 @@ package com.spiritlight.mobkilltracker.v3.utils.math;
 
 import java.math.BigDecimal;
 
-/** Utility class to do some maths. All methods use the {@code strictfp} modifier. */
+/** Utility class to do some maths. */
 public class StrictMath {
 
-    public static strictfp double decimalPointOf(double val) {
+    public static double decimalPointOf(double val) {
         return BigDecimal.valueOf(val).subtract(BigDecimal.valueOf((int) val)).doubleValue();
     }
 
-    public static strictfp double subtract(double a, double b) {
+    public static double subtract(double a, double b) {
         return BigDecimal.valueOf(a).subtract(BigDecimal.valueOf(b)).doubleValue();
     }
 
-    public static strictfp double add(double a, double b) {
+    public static double add(double a, double b) {
         return BigDecimal.valueOf(a).add(BigDecimal.valueOf(b)).doubleValue();
     }
 }
